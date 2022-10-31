f02f82a23bb3df62152fced6334bfbd382b0e3ff
Ivy Nguyen <animeweirdo24@gmail.com>
2022-10-31 15:35:10 -0600

    error handle bad network requests

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,13 +6,24 @@ import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
   const [news, setNews] = useState([])
+  const [errorOccured, setErrorOccured] = useState(false)
   
   const fetchData = () => {
     fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=tuGQcmG8QUpUgvlq0aIEgXlwWjoybiFL`)
-    .then(res => res.json())
+    .then(res => {
+      if(!res.ok) {
+        throw new Error()
+      } else {
+        setErrorOccured(false)
+        return res.json()
+      }
+    })
     .then(data => {
       console.log(data.results)
       setNews(data.results)
+    })
+    .catch(error => {
+      setErrorOccured(true)
     })
   }
 
@@ -20,10 +31,20 @@ const App = () => {
     console.log('section', section)
     if(section) {
       fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=tuGQcmG8QUpUgvlq0aIEgXlwWjoybiFL`)
-      .then(res => res.json())
+      .then(res => {
+        if(!res.ok) {
+          throw new Error()
+        } else {
+          setErrorOccured(false)
+          return res.json()
+        }
+      })
       .then(data => {
         console.log('searched section', data)
         setNews(data.results)
+      })
+      .catch(error => {
+        setErrorOccured(true)
       })
     }
   }
@@ -35,6 +56,7 @@ const App = () => {
   return (
     <div>
       <Switch>
+        {errorOccured && <h1>Error 404. We couldn't get the scoop! Please reload and try again</h1>}
         <Route exact path="/" render={() => <Main news={news} findNews={findNews}/>}/>
         <Route exact path="/scoop/:id" render={({match}) => {
               const foundArticle = news.find(article => article.uri.split("/")[3] === match.params.id)
